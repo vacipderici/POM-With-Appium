8a9bdd1f6b318d6b60fc8ed855aa2f656b461667
Vacip Derici <dericivd@gmail.com>
2022-07-03 18:43:58 +0300

    Data Driven Tests and Test Data in Json

--- AppiumPageObject/src/test/java/ToDo_Android.java
+++ AppiumPageObject/src/test/java/ToDo_Android.java
@@ -1,15 +1,28 @@
+import PageObjects.CreateTaskPage;
+import PageObjects.TasksListPage;
+import org.json.simple.parser.ParseException;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pageObject.CreateTaskPage;
 import pageObject.TaskListPage;
+import utils.JsonReader;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 
 public class ToDo_Android extends TestBase {
 
-    CreateTaskPage createTaskPage ;
+    CreateTaskPage createTaskPage;
     TaskListPage tasksListPage;
 
-    @Test
+    @DataProvider(name = "tasks data")
+    public Object[][] passData() throws IOException, ParseException {
+        return JsonReader.getJSONdata
+                (System.getProperty("user.dir") + "/data/TasksData.json"
+                        , "Tasks Data", 2);
+    }
+
+    @Test(dataProvider = "tasks data")
     public void test_add_task() throws MalformedURLException {
         Android_setUp();
         tasksListPage = new TaskListPage(driver);
@@ -17,6 +30,7 @@ public class ToDo_Android extends TestBase {
         tasksListPage.clickAddTaskBtn();
         createTaskPage.enterTaskName("Finish Appium Course");
         createTaskPage.enterTaskDesc("Finishing my course ASAP");
+        driver.hideKeyboard();
         createTaskPage.clickSaveBtn();
         tearDown();
     }
