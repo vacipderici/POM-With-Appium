eb53c2c45a771e3022bb1d8d60e7316874cadad1
Vacip Derici <dericivd@gmail.com>
2022-07-28 20:08:14 +0300

    Adding a Mapper to the Project

---
+++ AppiumPageObject/src/main/java/CreateNewTask.java
@@ -0,0 +1,14 @@
+public class CreateNewTask {
+    public CreateNewTask() {
+        Given("^Click Add new Task$", () -> {
+        });
+        Given("^Enter TaskName$", () -> {
+        });
+        Given("^Enter TaskDesc$", () -> {
+        });
+        When("^Click Save$", () -> {
+        });
+        Then("^Task added successfully$", () -> {
+        });
+    }
+}
